61c9ee6535ac24596e3a98207970617464438313
Rahul Jat <63182965+erlumbharam@users.noreply.github.com>
2021-01-25 23:25:15 +0530

    Create EXAMPLE.rb

---
+++ EXAMPLE.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+x, y, z = 12, 36, 72
+puts "The value of x is #{ x }."
+puts "The sum of x and y is #{ x + y }."
+puts "The average was #{ (x + y + z)/3 }."
+
+#output
+#The value of x is 12.
+#The sum of x and y is 48.
+#The average was 40.
